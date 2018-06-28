e6eed18dcac87e954938913897bc969e55350722
tanliangbang <648103576@qq.com>
2018-06-28 16:11:14 +0800

    2018-6-28

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './css/index.scss'
+import './js/jquery'
+import './js/index'
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,50 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 module.exports = {
     devtool: 'eval-source-map',
-    entry:  __dirname + "/src/js/main.js",
+    entry:  __dirname + "/src/main.js",
     output: {
         path: __dirname + "/dist",
-        filename: "main.js"
+        filename: "main-[hash].js"
     },
-
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+                loader: "babel-loader"
+            },
+            exclude: /node_modules/
+        },{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    'sass-loader'
+                ]
+            })
+        }]
+    },
+    devtool: 'eval-source-map',
     devServer: {
         contentBase: "./src",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
         inline: true//实时刷新
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/index.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new ExtractTextPlugin("style.css"),
+        new CleanWebpackPlugin('dist/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
+    ],
 }
